b3dead0328bd341441aac37ab7d09bf42bf60823
watagit <wtr0414@gmail.com>
2023-02-18 11:24:26 +0900

    feat: create Header

--- src/component/page/Top/Top.page.tsx
+++ src/component/page/Top/Top.page.tsx
@@ -1,6 +1,12 @@
 import type { FC } from "react";
 import { Top } from "./Top";
+import { Header } from "@/component/ui/Header";
 
 export const TopPage: FC = () => {
-  return <Top />;
+  return (
+    <>
+      <Header />
+      <Top />
+    </>
+  );
 };
