ab10c85dad8ddb05ab8d283fcf35492831f6b9f4
watagit <wtr0414@gmail.com>
2023-02-18 12:48:40 +0900

    fix: update path to article detail page

--- src/component/model/article/ArticleList/ArticleItem.tsx
+++ src/component/model/article/ArticleList/ArticleItem.tsx
@@ -12,7 +12,7 @@ export const ArticleItem: FC<ArticleItemProps> = ({ article }) => {
   return (
     <Link
       className="flex flex-col items-center gap-2 py-10 rounded border border-gray-200 duration-300 hover:scale-[102%]"
-      href={pagesPath.articles._id(article.slug).$url()}
+      href={pagesPath.articles._slug(article.slug).$url()}
     >
       <p className="text-5xl">{article.icon.value}</p>
       <h3 className="text-xs font-bold text-black">{article.title}</h3>
